8d05cf62dac46c29b713db58444e0c117e767cfa
haerinycutesy <haerinycutesy@gmail.com>
2026-07-11 06:56:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const letter = document.getElementById("letter");
+
+document.getElementById("openLetter").onclick=function(){
+
+letter.style.display="block";
+
+}
+
+document.getElementById("closeLetter").onclick=function(){
+
+letter.style.display="none";
+
+}
