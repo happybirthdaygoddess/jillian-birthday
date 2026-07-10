887b09a1289b0ab6d50d0bcf675f4867ace172b5
hbd <haerinycutesy@gmail.com>
2026-07-11 07:41:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
+// Letter
 const letter = document.getElementById("letter");
 
-document.getElementById("openLetter").onclick=function(){
+document.getElementById("openLetter").onclick = function () {
+    letter.style.display = "block";
+};
 
-letter.style.display="block";
+document.getElementById("closeLetter").onclick = function () {
+    letter.style.display = "none";
+};
 
-}
+// Music
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
 
-document.getElementById("closeLetter").onclick=function(){
+let playing = false;
 
-letter.style.display="none";
+// Start music after the first click anywhere on the page
+document.addEventListener(
+    "click",
+    () => {
+        if (!playing) {
+            music.play().then(() => {
+                playing = true;
+                musicBtn.textContent = "🔇 Pause Music";
+            }).catch(err => {
+                console.log("Music couldn't start:", err);
+            });
+        }
+    },
+    { once: true }
+);
 
-}
+// Toggle music button
+musicBtn.addEventListener("click", () => {
+
+    if (music.paused) {
+
+        music.play();
+        musicBtn.textContent = "🔇 Pause Music";
+
+    } else {
+
+        music.pause();
+        musicBtn.textContent = "🔊 Play Music";
+
+    }
+
+});
